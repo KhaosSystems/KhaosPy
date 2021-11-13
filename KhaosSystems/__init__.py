6f3a77a8300d6be3c182563380d3e401f96fa499
Storm Søndergaard <stormsoendergaard@gmail.com>
2021-11-13 16:41:27 +0100

    Work on command interpreters

--- KhaosSystems/__init__.py
+++ KhaosSystems/__init__.py
@@ -1,3 +1,3 @@
 from .KSNodeGraph import KSNodeItem, KSNodeGraph, KSNodeScene
-from .KSCommandInterpreter import KSCommandInterpreter, KSMayaCommandInterpreter
+from .KSCommandInterpreter import KSCommandInterpreter, KSMayaCommandInterpreter, KSEntityHandle, KSMayaEntityHandle
 from .KSBasicTypes import KSVector
